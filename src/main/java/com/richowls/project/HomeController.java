275eea1d8b9031a0a15f18e078eebf80c938f101
frozenrainyoo <frozenrain.yoo@gmail.com>
2015-12-31 11:29:34 +0900

    first_commit

---
+++ src/main/java/com/richowls/project/HomeController.java
@@ -0,0 +1,164 @@
+package com.richowls.project;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.richowls.project.dao.ContentDao;
+import com.richowls.project.dao.IDao;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+/*	ContentDao dao;*/
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+/*	@Autowired
+	public void setDao(ContentDao dao) {
+		this.dao = dao;
+	}*/
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		model.addAttribute("serverTime", formattedDate );
+//		
+//		return "home";
+//	}
+	
+	@RequestMapping("/list")
+	public String list(Model model) {
+//		ArrayList<ContentDto> dtos = dao.listDao();
+		IDao dao = sqlSession.getMapper(IDao.class);
+//		ArrayList<ContentDto> dtos = dao.listDao();
+		model.addAttribute("list", dao.listDao());
+		
+		return "/list";
+	}
+	
+	@RequestMapping("/writeForm")
+	public String writeForm() {
+		
+		return "/writeForm";
+	}
+	
+	@RequestMapping("/write")
+	public String write(HttpServletRequest request, Model model) {
+		IDao dao = sqlSession.getMapper(IDao.class);
+		dao.writeDao(request.getParameter("mWriter"), request.getParameter("mContent"));
+		return "redirect:list";
+	}
+	
+	@RequestMapping("/view")
+	public String view() {
+		
+		return "/view";
+	}
+	
+	@RequestMapping("/delete")
+	public String delete(HttpServletRequest request, Model model) {
+		IDao dao = sqlSession.getMapper(IDao.class);
+		dao.deleteDao(request.getParameter("mId"));
+		return "redirect:list";
+	}
+
+//	@RequestMapping("/login.html")
+//	public String login(Locale locale, Model model) {
+//		return "security/login";
+//	}
+//
+//	@RequestMapping("/welcome.html")
+//	public String welcome(Locale locale, Model model) {
+//		return "security/welcome";
+//	}
+
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView defaultPage() {
+
+	  ModelAndView model = new ModelAndView();
+	  model.addObject("title", "Spring Security Login Form - Database Authentication");
+	  model.addObject("message", "This is default page!");
+	  model.setViewName("hello");
+	  return model;
+
+	}
+
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+	  ModelAndView model = new ModelAndView();
+	  model.addObject("title", "Spring Security Login Form - Database Authentication");
+	  model.addObject("message", "This page is for ROLE_ADMIN only!");
+	  model.setViewName("admin");
+	  return model;
+
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+		@RequestParam(value = "logout", required = false) String logout) {
+
+	  ModelAndView model = new ModelAndView();
+	  if (error != null) {
+		model.addObject("error", "Invalid username and password!");
+	  }
+
+	  if (logout != null) {
+		model.addObject("msg", "You've been logged out successfully.");
+	  }
+	  model.setViewName("login");
+
+	  return model;
+
+	}
+	
+	//for 403 access denied page
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public ModelAndView accesssDenied() {
+
+	  ModelAndView model = new ModelAndView();
+		
+	  //check if user is login
+	  Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	  if (!(auth instanceof AnonymousAuthenticationToken)) {
+		UserDetails userDetail = (UserDetails) auth.getPrincipal();	
+		model.addObject("username", userDetail.getUsername());
+	  }
+		
+	  model.setViewName("403");
+	  return model;
+
+	}
+}
